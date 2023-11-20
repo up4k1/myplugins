f50aeb46d399ffc3696c413854da23ec863c0590
up4k <mailbox73@mail.ru>
2023-11-20 04:44:19 +0400

    d

---
+++ mass-remove-links-master/README.md
@@ -0,0 +1,20 @@
+# Plugin for YOURLS: **Mass Remove Links** [![Listed in Awesome YOURLS!](https://img.shields.io/badge/Awesome-YOURLS-C5A3BE)](https://github.com/YOURLS/awesome-yourls/)
+
+## What for
+
+Search for links based on various criteria (creation date, long URL...) and delete them. Handy when you want to delete dozens (or hundreds) of links at once.
+
+## How to
+
+* In `/user/plugins`, create a new folder named `mass-remove-links`
+* Drop these files in that directory
+* Go to the Plugins administration page and activate the plugin 
+* Have fun
+
+## Disclaimer
+
+Deleting your links will delete them.
+
+## License
+
+Do whatever the hell you want with this.
---
+++ telegram_multi/plugin.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: Advanced Telegram Notifier
+Description: Sends notifications to Telegram for different actions
+Version: 1.0
+Author: Your Name
+*/
+
+// Register admin page
+yourls_add_action('plugins_loaded', 'advanced_telegram_notifier_admin_page');
+function advanced_telegram_notifier_admin_page()
+{
+    yourls_register_plugin_page('advanced_telegram_notifier', 'Advanced Telegram Notifier', 'advanced_telegram_notifier_do_page');
+}
+
+// Display admin page
+function advanced_telegram_notifier_do_page()
+{
+    // Check if a form was submitted
+    if (isset($_POST['submit'])) {
+        // Process form and save options
+        yourls_verify_nonce('advanced_telegram_notifier_nonce');
+        yourls_update_option('advanced_telegram_notifier_token', trim($_POST['token']));
+        yourls_update_option('advanced_telegram_notifier_chat_id', trim($_POST['chat_id']));
+
+        $options = [
+            'notify_new_link' => isset($_POST['notify_new_link']) ? 'true' : 'false',
+            'notify_redirect' => isset($_POST['notify_redirect']) ? 'true' : 'false'
+        ];
+        yourls_update_option('advanced_telegram_notifier_options', $options);
+    }
+
+    // Get current options
+    $token = yourls_get_option('advanced_telegram_notifier_token');
+    $chat_id = yourls_get_option('advanced_telegram_notifier_chat_id');
+    $current_options = yourls_get_option('advanced_telegram_notifier_options', ['notify_new_link' => 'false', 'notify_redirect' => 'false']);
+
+    // Create nonce
+    $nonce = yourls_create_nonce('advanced_telegram_notifier_nonce');
+
+    // Build and display the admin page form
+    echo '<h2>Advanced Telegram Notifier Settings</h2>';
+    echo '<form method="post">';
+    echo '<input type="hidden" name="nonce" value="' . $nonce . '" />';
+    echo '<p><label for="token">Telegram Bot Token: </label> <input type="text" id="token" name="token" value="' . htmlspecialchars($token) . '" /></p>';
+    echo '<p><label for="chat_id">Telegram Chat ID: </label> <input type="text" id="chat_id" name="chat_id" value="' . htmlspecialchars($chat_id) . '" /></p>';
+    echo '<p><input type="checkbox" id="notify_new_link" name="notify_new_link" ' . ($current_options['notify_new_link'] === 'true' ? 'checked' : '') . '> <label for="notify_new_link">Notify on new link creation</label></p>';
+    echo '<p><input type="checkbox" id="notify_redirect" name="notify_redirect" ' . ($current_options['notify_redirect'] === 'true' ? 'checked' : '') . '> <label for="notify_redirect">Notify on link redirection</label></p>';
+    echo '<p><input type="submit" name="submit" value="Save" /></p>';
+    echo '</form>';
+}
+
+// Function to send notification
+function telegram_send_notification($message)
+{
+    $token = yourls_get_option('advanced_telegram_notifier_token');
+    $chat_id = yourls_get_option('advanced_telegram_notifier_chat_id');
+
+    $telegram_api_url = "https://api.telegram.org/bot" . $token . "/sendMessage";
+    $content = [
+        'chat_id' => $chat_id,
+        'text' => $message
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $telegram_api_url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($content));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if (!empty($response)) {
+        error_log('Telegram notification failed: ' . $response);
+    }
+}
+
+// Function to handle new link creation notification
+function advanced_telegram_notifier_on_new_link($args)
+{
+    $options = yourls_get_option('advanced_telegram_notifier_options');
+    if ($options['notify_new_link'] === 'true') {
+        $url = $args[1];
+        $shorturl = $args[2];
+        $message = "New link created: $url (Short URL: $shorturl)";
+        telegram_send_notification($message);
+    }
+}
+
+// Function to handle link redirection notification
+function advanced_telegram_notifier_on_redirect($args)
+{
+    $options = yourls_get_option('advanced_telegram_notifier_options');
+    if ($options['notify_redirect'] === 'true') {
+        $shorturl = $args[0];
+        $url = $args[1];
+        $message = "Short URL redirect: $shorturl to $url";
+        telegram_send_notification($message);
+    }
+}
+
+// Add actions
+yourls_add_action('insert_link', 'advanced_telegram_notifier_on_new_link');
+yourls_add_action('redirect_shorturl', 'advanced_telegram_notifier_on_redirect');
